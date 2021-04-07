0aafe5679e47bef56974464e0bacdf758abf1c8e
Carbon225 <carbon225@protonmail.com>
2021-04-07 17:01:24 +0200

    new merge

--- lab4/merge.c
+++ lab4/merge.c
@@ -1,35 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int parent(int i)
+{
+  return (i - 1) / 2;
+}
+
+int left(int i)
+{
+  return 2 * i + 1;
+}
+
+int right(int i)
+{
+  return 2 * i + 2;
+}
+
+void heapify(int *A[], int n, int i)
+{
+  int m = i;
+  do
+  {
+    i = m;
+    int l = left(m);
+    int r = right(m);
+
+    if (l < n && *A[l] < *A[m])
+      m = l;
+
+    if (r < n && *A[r] < *A[m])
+      m = r;
+
+    int *tmp = A[i];
+    A[i] = A[m];
+    A[m] = tmp;
+  } while (m != i);
+}
+
+void build_heap(int *A[], int n)
+{
+  for (int i = parent(n - 1); i >= 0; --i)
+    heapify(A, n, i);
+}
+
 int main()
 {
   int n;
   scanf("%d", &n);
+  int N = n;
 
-  int *t = malloc(n * n * sizeof(*t));
-
+  int *t = malloc(n * (n + 1) * sizeof(*t));
   int **rows = malloc(n * sizeof(*rows));
+
   for (int y = 0; y < n; ++y)
-    rows[y] = t + n * y;
-
-  for (int i = 0; i < n * n; ++i)
-      scanf("%d", t + i);
-
-  int *last_value = 0;
-  for (;;)
   {
-    int **max_row = 0;
-    for (int y = 0; y < n; ++y)
-      if (rows[y] < t + y * n + n && (max_row == 0 || rows[y][0] < **max_row))
-        max_row = rows + y;
+    for (int x = 0; x < n; ++x)
+      scanf("%d", t + y * (n + 1) + x);
+    rows[y] = t + y * (n + 1);
+  }
 
-    if (max_row == 0)
-      break;
+  build_heap(rows, n);
 
-    if (last_value == 0 || **max_row != *last_value)
-      printf("%d ", **max_row);
-    last_value = *max_row;
-    ++*max_row;
+  int *last_print = 0;
+
+  while (n > 0)
+  {
+    if (last_print == 0 || *rows[0] != *last_print)
+      printf("%d ", *rows[0]);
+    last_print = rows[0];
+
+    rows[0] += 1;
+
+    if ((rows[0] - t) % (N + 1) == N)
+    {
+      n -= 1;
+      rows[0] = rows[n];
+    }
+
+    heapify(rows, n, 0);
   }
 
   printf("\n");
