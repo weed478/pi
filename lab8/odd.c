ea565090b60e45bc4c1be41a39aa85edd141686c
Carbon225 <carbon225@protonmail.com>
2021-05-11 12:21:29 +0200

    biggest brain

--- lab8/odd.c
+++ lab8/odd.c
@@ -2,70 +2,28 @@
 
 typedef long long num_t;
 
-num_t f(num_t x)
+num_t odd_numbers_upto(num_t n)
 {
-    while (x % 2 == 0)
-        x /= 2;
-    return x;
+    return ((n + 1) / 2) * ((n + 1) / 2);
 }
 
-num_t naive(num_t N)
+num_t odd_numbers_between(num_t a, num_t b)
 {
-    num_t res = 0;
-    for (num_t i = 1; i <= N; i++)
-    {
-        res += f(i);
-    }
-
-    return res;
+    return odd_numbers_upto(b) - odd_numbers_upto(a - 1);
 }
 
-int div_count(num_t N, num_t a0)
+num_t biggest_brain(num_t N)
 {
-    // to samo co floor(log2(2N/a0))
-    num_t an = a0;
-    int n = 0;
-    while (an <= N)
-    {
-        an *= 2;
-        n++;
-    }
-    return n;
-}
+    num_t res = odd_numbers_upto(N);
 
-num_t big_brain(num_t N)
-{
-    num_t res = 0;
-
-    for (num_t i = 1; i <= N; i += 2)
-    {
-        res += div_count(N / 2, i) * i;
-        res += i;
-    }
-
-    // moment realizacji ze to wlasciwie robi dokladnie to samo
-
-    return res;
-}
-
-num_t bigger_brain(num_t N)
-{
-    num_t res = 0;
-    int count = 0;
+    int count = 1;
     num_t count_break = N / 2;
 
-    for (num_t i = N; i >= 1; --i)
+    while (count_break >= 1)
     {
-        if (i <= count_break)
-        {
-            count++;
-            count_break /= 2;
-        }
-
-        if (i % 2 == 0)
-            continue;
-
-        res += count * i + i;
+        res += count * odd_numbers_between(count_break / 2 + 1, count_break);
+        count_break /= 2;
+        count++;
     }
 
     return res;
@@ -75,6 +33,6 @@ int main()
 {
     num_t N;
     scanf("%lld", &N);
-    printf("%lld\n", bigger_brain(N));
+    printf("%lld\n", biggest_brain(N));
     return 0;
 }
