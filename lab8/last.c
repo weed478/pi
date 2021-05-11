1e33917327efc80c1305814d0f85f1261407e475
Carbon225 <carbon225@protonmail.com>
2021-05-11 18:14:49 +0200

    lastdigit

--- lab8/last.c
+++ lab8/last.c
@@ -1,4 +1,34 @@
+#include <stdio.h>
+
+typedef long long num_t;
+
 int main()
 {
+    num_t n;
+    scanf("%lld", &n);
+
+    num_t base = 1;
+    while (n / base != 0)
+        base = base * 10 + 1;
+    base /= 10;
+
+    num_t X = 0;
+    while (base > 0)
+    {
+        X *= 10;
+
+        if (n / base > 9)
+        {
+            X = -1;
+            break;
+        }
+
+        X += n / base;
+        n = n % base;
+        base /= 10;
+    }
+
+    printf("%lld\n", X);
+
     return 0;
 }
