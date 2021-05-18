fed405847e5514e59fd7f7398e4ec22411222bd3
Carbon225 <carbon225@protonmail.com>
2021-05-19 00:42:26 +0200

    semi

--- lab9/semi.c
+++ lab9/semi.c
@@ -1,4 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+typedef int_fast32_t num_t;
+
 int main()
 {
+    int n;
+    num_t m;
+    scanf("%d %" SCNdFAST32, &n, &m);
+
+    int count = 0;
+
+    for (num_t x = 1; x < 1 << n; ++x)
+    {
+        if (x % m == 0)
+            continue;
+
+        num_t mask = 1;
+        for (int bit = 0; bit < n; ++bit, mask <<= 1)
+        {
+            num_t y = x ^ mask;
+            if (y != 0 && y % m == 0)
+            {
+                count++;
+                break;
+            }
+        }
+    }
+
+    printf("%d\n", count);
+
     return 0;
 }
