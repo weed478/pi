ff14ffb57d5c403ddaef16a8fe0c8765912ac20e
Carbon225 <carbon225@protonmail.com>
2021-05-27 20:25:21 +0200

    ban

--- lab10/ban.c
+++ lab10/ban.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <stdbool.h>
-#include <inttypes.h>
+#include <string.h>
 
 #define N_ATM (100)
+#define N_DIGITS (64)
 
-void solve(uint64_t x, bool flip_control)
+void solve(const bool C[], bool flip_control)
 {
     bool borrow[N_ATM + 1];
     borrow[N_ATM] = false;
     bool A[N_ATM];
     bool B[N_ATM];
-    bool C[N_ATM];
 
     for (int i = 0; i < N_ATM; ++i)
     {
         borrow[i] = false;
         A[i] = 0;
         B[i] = 0;
-        C[i] = x & 1;
-        x >>= 1;
     }
 
     for (int i = 0; i < N_ATM; ++i)
@@ -121,22 +118,71 @@ void solve(uint64_t x, bool flip_control)
     }
 }
 
-void solve_withdrawal(uint64_t x)
+void solve_withdrawal(const bool C[])
 {
-    solve(x, false);
+    solve(C, false);
 }
 
-void solve_deposit(uint64_t x)
+void solve_deposit(const bool C[])
 {
-    solve(x, true);
+    solve(C, true);
+}
+
+int char2int(char c)
+{
+    return c - '0';
+}
+
+char int2char(int i)
+{
+    return '0' + i;
+}
+
+int div2(char num[])
+{
+    int remainder = 0;
+    for (int i = 0; i < N_DIGITS; ++i)
+    {
+        remainder *= 10;
+        remainder += char2int(num[i]);
+        num[i] = int2char(remainder / 2);
+        remainder %= 2;
+    }
+    return remainder;
+}
+
+void char_to_bits(char number[], bool bits[])
+{
+    for (int i = 0; i < N_ATM; ++i)
+    {
+        bits[i] = div2(number);
+    }
+}
+
+void rjustify(char num[])
+{
+    int offset = N_DIGITS - strnlen(num, N_DIGITS);
+
+    for (int i = N_DIGITS - 1; i >= offset; --i)
+        num[i] = num[i - offset];
+
+    for (int i = 0; i < offset; ++i)
+        num[i] = '0';
 }
 
 int main()
 {
-    uint64_t x;
-    scanf("%" SCNu64, &x);
-    solve_withdrawal(x);
-    solve_deposit(x);
+    char number[N_DIGITS + 1];
+    number[N_DIGITS] = 0;
+
+    scanf("%s", number);
+    rjustify(number);
+
+    bool bits[N_ATM];
+    char_to_bits(number, bits);
+
+    solve_withdrawal(bits);
+    solve_deposit(bits);
 
     return 0;
 }
